4ed4b40c4fb406b102a53d55b0c8978e190644a8
Mirna Andrišić <mandrisic@ffos.hr>
2022-08-29 10:00:05 +0200

    Update Match.js

--- src/Match.js
+++ src/Match.js
@@ -4,104 +4,23 @@ import './Match.css';
 import Nav from './Nav';
 import igracKamen from './slike/igrac/kamen.png';
 import compKamen from './slike/comp/kamen.png';
+/*
+import igracPapir from './slike/igrac/papir.png';
+import compPapir from './slike/comp/papir.png';
+import igracSkare from './slike/igrac/skare.png';
+import compSkare from './slike/comp/skare.png';
+*/
 
 const Match = (props) => {
-  let pScore = 0;
-  let cScore = 0;
-
-  const updateScore = () => {
-    const playerScore = document.querySelector('.player-score p');
-    const computerScore = document.querySelector('.computer-score p');
-    playerScore.textContent = pScore;
-    computerScore.textContent = cScore;
-  };
-
-  const playMatch = () => {
-    const options = document.querySelectorAll('.options button');
-    const playerHand = document.querySelector('.playerHand');
-    const computerHand = document.querySelector('.computerHand');
-    const hands = document.querySelectorAll('.hands img');
-
-    hands.forEach(hand => {
-      hand.addEventListener('animationend', function() {
-        this.style.animation = '';
-      });
-    });
-
-    const computerOptions = ['kamen', 'papir', 'skare'];
-
-    options.forEach(option => {
-      option.addEventListener('click', function() {
-        const computerNumber = Math.floor(Math.random() * 3);
-        const computerChoice = computerOptions[computerNumber];
-
-        setTimeout(() => {
-          compareHands(this.textContent, computerChoice);
-
-          playerHand.src = `./slike/igrac/${this.textContent}.png`;
-          computerHand.src = `./slike/comp/${computerChoice}.png`;
-        }, 2000);
-
-        playerHand.style.animation = 'shakePlayer 2s ease';
-        computerHand.style.animation = 'shakeComputer 2s ease';
-      });
-    });
-  };
-
-  const compareHands = (playerChoice, computerChoice) => {
-    const winner = document.querySelector('.winner');
-
-    if (playerChoice === computerChoice) {
-      winner.textContent = 'Neriješeno';
-      return;
-    }
-
-    if (playerChoice === 'kamen') {
-      if (computerChoice === 'skare') {
-        winner.textContent = 'Igrač pobjeđuje';
-        pScore++;
-        updateScore();
-        return;
-      } else {
-        winner.textContent = 'Računalo pobjeđuje';
-        cScore++;
-        updateScore();
-        return;
-      }
-    }
-
-    if (playerChoice === 'papir') {
-      if (computerChoice === 'skare') {
-        winner.textContent = 'Računalo pobjeđuje';
-        cScore++;
-        updateScore();
-        return;
-      } else {
-        winner.textContent = 'Igrač pobjeđuje';
-        pScore++;
-        updateScore();
-        return;
-      }
-    }
-
-    if (playerChoice === 'skare') {
-      if (computerChoice === 'kamen') {
-        winner.textContent = 'Računalo pobjeđuje';
-        cScore++;
-        updateScore();
-        return;
-      } else {
-        winner.textContent = 'Igrač pobjeđuje';
-        pScore++;
-        updateScore();
-        return;
-      }
-    }
-  };
-
-  playMatch();
 
   const navigate = useNavigate();
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    console.log('The link was clicked.');
+
+  }
+
   return (
     <div className="Nav">
     <Nav />
@@ -114,7 +33,7 @@ const Match = (props) => {
       </div>
 
       <div className="options">
-          <button className="kamen">kamen</button>
+          <button className="kamen" onClick={handleClick}>kamen</button>
           <button className="papir">papir</button>
           <button className="skare">skare</button>
         </div>
